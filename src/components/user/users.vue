b88cb5e3085800b078ca0fba0c7d3d9e28735e8f
nogodfyb <120626481@qq.com>
2020-05-07 18:29:43 +0800

    add addUserForm feature delete log

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -57,10 +57,10 @@
     <el-dialog
       title="添加用户"
       :visible.sync="dialogVisible"
-      width="50%"
+      width="50%" @close="addDialogClosed"
       >
 <!--      内容主体区域-->
-      <el-form :model="addForm" :rules="addFormRules" ref="addRuleForm" label-width="70px" >
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px" >
         <el-form-item label="用户名" prop="username">
           <el-input v-model="addForm.username"></el-input>
         </el-form-item>
@@ -77,7 +77,7 @@
 <!--      底部区域-->
       <span slot="footer" class="dialog-footer">
     <el-button @click="dialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    <el-button type="primary"  @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
   </el-card>
@@ -102,7 +102,7 @@ export default {
     // 验证手机号的规则
     const checkMobile = (rule, value, cb) => {
       // 验证手机号的正则表达式
-      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      const regMobile = /^1[3456789]\d{9}$/
 
       if (regMobile.test(value)) {
         return cb()
@@ -163,12 +163,10 @@ export default {
       console.log(res)
     },
     handleSizeChange (newSize) {
-      console.log(newSize)
       this.queryInfo.pageSize = newSize
       this.getUserList()
     },
     handleCurrentChange (newPage) {
-      console.log(newPage)
       this.queryInfo.pageNum = newPage
       this.getUserList()
     },
@@ -182,6 +180,27 @@ export default {
         return this.$message.error('更新用户状态失败！')
       }
       this.$message.success('更新用户状态成功！')
+    },
+    addDialogClosed () {
+      // 不需要自己清空数据 以下不仅清空了addForm的数据并且清空了验证遗留信息
+      this.$refs.addFormRef.resetFields()
+    },
+    addUser () {
+      this.$refs.addFormRef.validate(async (valid) => {
+        if (!valid) {
+          return false
+        }
+        // 可以发起添加用户的网络请求
+        const { data: res } = await this.$http.post('user/users', this.addForm)
+        if (res.status !== 201) {
+          this.$message.error('添加用户失败！')
+        }
+        this.$message.success('添加用户成功！')
+        // 隐藏添加用户的对话框
+        this.dialogVisible = false
+        // 重新获取用户列表数据
+        this.getUserList()
+      })
     }
   }
 }

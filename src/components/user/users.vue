fcd5e00d2adc1e437a09e0ee5c5e5fc34eee8029
nogodfyb <120626481@qq.com>
2020-05-07 17:14:18 +0800

    add addUserForm feature

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -16,7 +16,7 @@
       </el-input>
       </el-col>
       <el-col :span="4">
-        <el-button type="primary">添加用户</el-button>
+        <el-button type="primary" @click="dialogVisible=true">添加用户</el-button>
       </el-col>
     </el-row>
     <!-- 用户列表区域 -->
@@ -53,6 +53,33 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total">
     </el-pagination>
+<!--    添加用户的对话框-->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="dialogVisible"
+      width="50%"
+      >
+<!--      内容主体区域-->
+      <el-form :model="addForm" :rules="addFormRules" ref="addRuleForm" label-width="70px" >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+      </el-form>
+<!--      底部区域-->
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </el-card>
 </div>
 </template>
@@ -60,6 +87,29 @@
 <script>
 export default {
   data () {
+    // 验证邮箱的规则
+    const checkEmail = (rule, value, cb) => {
+      // 验证邮箱的正则表达式
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+
+      if (regEmail.test(value)) {
+        // 合法的邮箱
+        return cb()
+      }
+
+      cb(new Error('请输入合法的邮箱'))
+    }
+    // 验证手机号的规则
+    const checkMobile = (rule, value, cb) => {
+      // 验证手机号的正则表达式
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+
+      if (regMobile.test(value)) {
+        return cb()
+      }
+
+      cb(new Error('请输入合法的手机号'))
+    }
     return {
     // 获取用户列表的参数对象
       queryInfo: {
@@ -70,7 +120,33 @@ export default {
         pageSize: 5
       },
       userList: [],
-      total: 0
+      total: 0,
+      // 用户对话框的可见性
+      dialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        mobile: '',
+        email: ''
+      },
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入用户密码', trigger: 'blur' },
+          { min: 5, max: 15, message: '长度在 5 到 15 个字符', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur', message: '请输入合法的邮箱' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur', message: '请输入而合法的手机号' }
+        ]
+      }
     }
   },
   created () {
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import {
   Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu,
   Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch,
-  Tooltip, Pagination
+  Tooltip, Pagination, Dialog
 } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
@@ -25,4 +25,5 @@ Vue.use(TableColumn)
 Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
